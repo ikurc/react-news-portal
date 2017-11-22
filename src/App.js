53ccc41721571e27a06d70edb27e40d953bc0ee8
Bogdan <bogdan.makarenko@gmail.com>
2017-11-22 15:45:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Header from './Header'
 import Publishers from './Publishers'
 import Subscribers from './Subscribers'
+import './App.css'
 
 class App extends Component {
     updateState = () => {
