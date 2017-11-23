541b4a8b195c66c3dd97024fa532d974ace23e62
Igor Kurchenko <kurchenkoigor1@gmail.com>
2017-11-23 12:50:20 +0200

    most final commit

--- src/view/components/Header/Header.js
+++ src/view/components/Header/Header.js
@@ -5,6 +5,7 @@ const Header = (props) => {
   return (
     <header className="header">
       <p className="portal-name">{props.portalName}</p>
+    <a className="portal-docs" href="https://github.com/scarigor/react-news-portal">GitHub</a>
     </header>
   )
 }
